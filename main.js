0ca9a82be24a45cf7f2da13ef9880b66f891672d
Jordan Royle <royle.jordan12@gmail.com>
2025-05-02 16:26:35 +0100

    Updated Website Code

---
+++ main.js
@@ -0,0 +1,14 @@
+// Select all images on the page
+const images = document.querySelectorAll('img');
+
+// Add hover effect to scale images
+images.forEach((img) => {
+    img.addEventListener('mouseover', () => {
+        img.style.transform = 'scale(2)';
+        img.style.transition = 'transform 0.3s ease';
+    });
+
+    img.addEventListener('mouseout', () => {
+        img.style.transform = 'scale(1)';
+    });
+});
